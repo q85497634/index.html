ea2d754897cdd7c427dfe37c0c394fd392fe6df4
q85497634 <40864142+q85497634@users.noreply.github.com>
2026-08-02 16:46:08 +0800

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -3,14 +3,27 @@ var title='somneone's Cloud';
 var sign='sign';
 <!--主页内容-->
 
-// 🔥 修正：适配新数组格式的分割线
 const blank = [{ name: '————————', icon: 'blank', url: '#none#' }];
 
 var arrA=new Array();
-// ===================== arrA 主页面数据【新数组格式】=====================
+// ===================== arrA 主页面数据==================
 arrA.push([
-  { name: '文件浏览器', icon: 'fbis', port: '38088' },
-  { name: 'IPV6', icon: 'wan', url: 'https://yourdomain:38088',type:'ipv6' },
-  { name: '局域网', icon: 'lan', url: 'https://192.168.1.1:38088',type:'lan' },
+  { name: '文件浏览器', icon: 'fbis', port: '8088' },
+  { name: 'IPV6', icon: 'wan', url: 'https://yourdomain:8088',type:'ipv6' },
+  { name: '局域网', icon: 'lan', url: 'https://192.168.1.1:8088',type:'lan' },
+  ...blank
+]):
+arrA.push([
+  { name: '文件浏览器', icon: 'fbis', port: '8088' },
+  { name: 'IPV6', icon: 'wan', url: 'https://yourdomain:8088',type:'ipv6' },
+  { name: '局域网', icon: 'lan', url: 'https://192.168.1.1:8088',type:'lan' },
+  ...blank
+]):
+var arrB=new Array();
+// ===================== arrB 副页面数据==================
+arrA.push([
+  { name: '文件浏览器', icon: 'fbis', port: '8088' },
+  { name: 'IPV6', icon: 'wan', url: 'https://yourdomain:8088',type:'ipv6' },
+  { name: '局域网', icon: 'lan', url: 'https://192.168.1.1:8088',type:'lan' },
   ...blank
 ]):
