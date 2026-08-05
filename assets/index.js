1b52925820474d17f7ba1e58d47a79cb1181c8f3
q85497634 <40864142+q85497634@users.noreply.github.com>
2026-08-05 12:18:39 +0800

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,37 +1,48 @@
-<!--标题及签名-->
-var title='somneone's Cloud';
-var sign='sign';
-<!--主页内容-->
-
 const blank = [{ name: '————————', icon: 'blank', url: '#none#' }];
-
 var arrA=new Array();
 // ===================== arrA 主页面数据==================
 arrA.push([
-  { name: '文件浏览器', icon: 'fbis', ports: '18088' },
-  { name: '文件浏览器', icon: 'fbis', port: '28088' },
-  { name: 'IPV6', icon: 'wan', url: 'https://yourdomain:8088',type:'ipv6' },
-  { name: 'IPV6', icon: 'wan', wan: 'https://yourdomain:8088',type:'ipv6' },
+   //示例①：ports和port，会生成对应的https://host:ports和https://host:port。host由内网或外网决定
+  { name: '文件浏览器(第一个对象为主图标)', icon: 'fbis', ports: '18088' },
+  { name: '文件浏览器(第二个及以后为弹出菜单图标)', icon: 'fbis', port: '28088' },
+  //添加blank生成一条分割线
   ...blank,
-  { name: '局域网', icon: 'lan', lan: 'https://192.168.1.1:8088',type:'lan' }
+  //示例②：url中填入${ddns}或${lan_addr}，会总是填入预设的内网ip/ddns域名。
+  { name: 'IPV6(第二个及以后为弹出菜单图标)', icon: 'wan', url: `https://${ddns}:18088`,type:'ipv6' },
+  { name: '局域网(第二个及以后为弹出菜单图标)', icon: 'lan', url: `https://${lan_addr}:28088`,type:'lan' }
 ]):
+//##############################
+//以上部分为主页面的第一个图标，其中第一个对象为主图标，第二个及以后为弹出菜单图标
+//name为显示的名称，icon为/img/png下的图标名，推荐128*128,必须为png格式，type决定了文字的显示样式，样式可在index.html中修改，其他参数（url,ports,port,lan,wan,）至少有一个就行
+//##############################
 arrA.push([
+  //示例③：ports和port后面可以跟/路径
   { name: 'Lucky', icon: 'lucky2', ports: '16601/safe' },
-  { name: 'IPV6', icon: 'wan', url: 'https://yourdomain:8088',type:'ipv6' },
-  { name: '局域网', icon: 'lan', url: 'https://192.168.1.1:8088',type:'lan' },
-  ...blank
+  ...blank,
+  { name: 'IPV6', icon: 'wan', url: `https://${ddns}:16601/safe`,type:'ipv6' },
+  { name: '局域网', icon: 'lan', url: `https://${lan_addr}:16601/safe`,type:'lan' }
 ]):
+//##############################
+//以上部分为主页面的第二个图标
+//##############################
+arrA.push([
+  //示例④：可以填入相对路径
+  { name: 'iStore', icon: 'is', url: '../cgi-bin/luci'},
+]);
 
 arrB.push([
   { name: 'NanoCloud', icon: 'clash', url: 'https://edu.360buyimg.men/auth/register?code=x1up9jm6' }
 ]);
 
-
+arrA.push([
+  //示例⑥：可以分别填写wan和lan地址,wan地址可写ddnsto的地址
+  { name: '电信光猫', icon: 'tele', 'wan': 'https://xxx.gd.ddnsto.com/cgi-bin/luci','lan': 'http://192.168.1.1' }
+]);
 
 var arrB=new Array();
 // ===================== arrB 副页面数据==================
 arrB.push([
-  { name: 'Github', icon: 'github2', url: 'https://github.com/q85497634?tab=stars',type:'vpn' },
+  { name: 'Github', icon: 'github2', url: 'https://github.com/q85497634/index.html',type:'vpn' },
   { name: '☑luci-app-lucky安装-aarch64_cortex-a53', icon: 'lucky', url: 'https://github.com/sirpdboy/luci-app-lucky/releases' },
   { name: 'luci-app-wechatpush-aarch64_cortex-a53', icon: 'github2', url: 'https://github.com/tty228/luci-app-wechatpush' },
   { name: 'luci-theme-argon', icon: 'argon', url: 'https://github.com/jerrykuku/luci-theme-argon' },
@@ -39,11 +50,11 @@ arrB.push([
 ]);
 
 arrB.push([
-  { name: 'DockerHub', icon: 'Docker_D', url: 'https://hub.docker.com/u/simon854/starred',type:'vpn' }
+  { name: 'DockerHub', icon: 'Docker_D', url: 'https://hub.docker.com',type:'vpn' }
 ]);
 
 arrB.push([
-  { name: 'CSDN', icon: 'csdn', url: 'https://www.csdn.net/i/collections?spm=1000.2115.3001.10430' }
+  { name: 'CSDN', icon: 'csdn', url: 'https://www.csdn.net' }
 ]);
 
 arrB.push([
@@ -65,10 +76,5 @@ arrB.push([
 arrB.push([
   { name: '酷友社', icon: 'kool', url: 'https://www.koolcenter.com' }
 ]);
-
-arrA.push([
-  { name: '文件浏览器', icon: 'fbis', port: '8088' },
-  { name: 'IPV6', icon: 'wan', url: 'https://yourdomain:8088',type:'ipv6' },
-  { name: '局域网', icon: 'lan', url: 'https://192.168.1.1:8088',type:'lan' },
   ...blank
 ]):
