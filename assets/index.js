6108b17c36e155853fd3a74a4d198b7143ba46fa
q85497634 <40864142+q85497634@users.noreply.github.com>
2026-08-05 11:42:19 +0800

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -8,19 +8,64 @@ const blank = [{ name: '————————', icon: 'blank', url: '#none#' 
 var arrA=new Array();
 // ===================== arrA 主页面数据==================
 arrA.push([
-  { name: '文件浏览器', icon: 'fbis', port: '8088' },
+  { name: '文件浏览器', icon: 'fbis', ports: '18088' },
+  { name: '文件浏览器', icon: 'fbis', port: '28088' },
   { name: 'IPV6', icon: 'wan', url: 'https://yourdomain:8088',type:'ipv6' },
-  { name: '局域网', icon: 'lan', url: 'https://192.168.1.1:8088',type:'lan' },
-  ...blank
+  { name: 'IPV6', icon: 'wan', wan: 'https://yourdomain:8088',type:'ipv6' },
+  ...blank,
+  { name: '局域网', icon: 'lan', lan: 'https://192.168.1.1:8088',type:'lan' }
 ]):
 arrA.push([
-  { name: '文件浏览器', icon: 'fbis', port: '8088' },
+  { name: 'Lucky', icon: 'lucky2', ports: '16601/safe' },
   { name: 'IPV6', icon: 'wan', url: 'https://yourdomain:8088',type:'ipv6' },
   { name: '局域网', icon: 'lan', url: 'https://192.168.1.1:8088',type:'lan' },
   ...blank
 ]):
+
+arrB.push([
+  { name: 'NanoCloud', icon: 'clash', url: 'https://edu.360buyimg.men/auth/register?code=x1up9jm6' }
+]);
+
+
+
 var arrB=new Array();
 // ===================== arrB 副页面数据==================
+arrB.push([
+  { name: 'Github', icon: 'github2', url: 'https://github.com/q85497634?tab=stars',type:'vpn' },
+  { name: '☑luci-app-lucky安装-aarch64_cortex-a53', icon: 'lucky', url: 'https://github.com/sirpdboy/luci-app-lucky/releases' },
+  { name: 'luci-app-wechatpush-aarch64_cortex-a53', icon: 'github2', url: 'https://github.com/tty228/luci-app-wechatpush' },
+  { name: 'luci-theme-argon', icon: 'argon', url: 'https://github.com/jerrykuku/luci-theme-argon' },
+  { name: 'luci-app-ddns-go', icon: 'ddnsgo', url: 'https://github.com/sirpdboy/luci-app-ddns-go' }
+]);
+
+arrB.push([
+  { name: 'DockerHub', icon: 'Docker_D', url: 'https://hub.docker.com/u/simon854/starred',type:'vpn' }
+]);
+
+arrB.push([
+  { name: 'CSDN', icon: 'csdn', url: 'https://www.csdn.net/i/collections?spm=1000.2115.3001.10430' }
+]);
+
+arrB.push([
+  { name: 'ddnsto', icon: 'ddnsto', url: 'https://fw.koolcenter.com/binary/ddnsto/openwrt/' }
+]);
+
+arrB.push([
+  { name: '浩思彼岸', icon: 'hassbian', url: 'https://bbs.hassbian.com/forum-38-1.html' }
+]);
+
+arrB.push([
+  { name: '矿神', icon: 'imnks', url: 'https://imnks.com/#google_vignette' }
+]);
+
+arrB.push([
+  { name: '恩山', icon: 'enshan', url: 'https://www.right.com.cn' }
+]);
+
+arrB.push([
+  { name: '酷友社', icon: 'kool', url: 'https://www.koolcenter.com' }
+]);
+
 arrA.push([
   { name: '文件浏览器', icon: 'fbis', port: '8088' },
   { name: 'IPV6', icon: 'wan', url: 'https://yourdomain:8088',type:'ipv6' },
